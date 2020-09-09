bc216337e82add6d30ca8b75aefb34fe78f622d9
francepants <franceline90@gmail.com>
2020-09-09 11:57:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '') 
+  }
+
+  static titleize(string) {
+    //capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from
+    // and always capitalizes the first word.
+    let exceptWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let array = string.split(" ")
+    let caps = array.map(word => {
+      if (!exceptWords.includes(word)) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    })
+    caps[0] = this.capitalize(caps[0])
+    return caps.join(" ")
+  }
 }
+
